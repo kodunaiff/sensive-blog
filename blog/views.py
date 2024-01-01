8e5215b365cc1e8d3704fe773a3b66d568693ec9
kodunaiff <kodunaiff@gmail.com>
2024-01-02 02:26:08 +0800

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -39,12 +39,9 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = Post.objects.popular() \
-                             .prefetch_related('author')[:5] \
-        .fetch_with_comments_count()
-
-    most_fresh_posts = Post.objects.prefetch_related('author') \
-                           .order_by('-published_at')[:5].fetch_with_comments_count()
+    all_posts = Post.objects.prefetch_related('author').prefetch_related('tags')
+    most_popular_posts = all_posts.popular()[:5].fetch_with_comments_count()
+    most_fresh_posts = all_posts.order_by('-published_at')[:5].fetch_with_comments_count()
 
     most_popular_tags = Tag.objects.popular()[:5]
 
